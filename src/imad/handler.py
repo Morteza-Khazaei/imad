03f7ef7b68ec9cf92f6f7bb6ca1f2f946840822c
Morteza-Khazaei <mortezakhazaei1370@gmail.com>
2023-08-31 17:05:43 +0200

    debug

--- src/imad/handler.py
+++ src/imad/handler.py
@@ -86,7 +86,7 @@ def main():
             all_files = os.listdir(l3a_path)
 
             # Filter files that have B2, B3, B4, B8
-            NRGB_bands = [fname for fname in all_files if any(band in fname for band in ['B2', 'B3', 'B4', 'B8'])]
+            NRGB_bands = [fname for fname in all_files if any(band in fname for band in ['B2', 'B3', 'B4', 'B8.'])]
 
             NRGB_file = create_geotiff(l3a_path, NRGB_bands)
             
