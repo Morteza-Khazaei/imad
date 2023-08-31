6d918184661dd5acb002263b2bd42dda645e6152
Morteza-Khazaei <mortezakhazaei1370@gmail.com>
2023-08-31 17:19:27 +0200

    debug

--- src/imad/handler.py
+++ src/imad/handler.py
@@ -27,6 +27,7 @@ def create_geotiff(base_dir, input_files):
     output_file = os.path.join(base_dir, fname)
 
     if not os.path.exists(output_file):
+        print(f'******* Write raster file with id: {fname}.')
         # Create a new dataset for the NRGB bands
         driver = gdal.GetDriverByName("GTiff")
         ds_out = driver.Create(output_file, bands[0].XSize, bands[0].YSize, len(bands), bands[0].DataType)
